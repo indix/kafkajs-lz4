f05f37dad544f432b4b14cd64ab0e5decb094234
GP <exchequer598@gmail.com>
2018-09-29 10:46:40 +0530

    Go back to function signature during calling.

--- src/index.ts
+++ src/index.ts
@@ -73,12 +73,10 @@ export default class LZ4Codec {
      * KafkaJS CompressionType-compatible LZ4 codec.
      * @memberof LZ4Codec
      */
-    public codec = () => {
-        return {
-            compress: this.compress,
-            decompress: this.decompress,
-        };
-    }
+    public codec = {
+        compress: this.compress,
+        decompress: this.decompress,
+    };
 }
 
 module.exports = LZ4Codec;
